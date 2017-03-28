c64cac0935cb0d23623f5dcbe9d74f0a65967270
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2017-03-28 10:01:40 +0000

    Drobne bledy

---
+++ src/structs/structures.h
@@ -0,0 +1,17 @@
+#ifndef GOL_C_STRUCTURES_H
+#define GOL_C_STRUCTURES_H
+
+#include <../game/map.h>
+
+typedef struct Structures {
+	char **names;
+	map_t *structs;
+	int structs_amount;
+} *structs_t;
+
+structs_t alloc_structs();
+int load_all_structs(structs_t structs);
+map_t load_structure(structs_t structs, char *name);
+int save_structure(structs_t structs, char *name);
+
+#endif
